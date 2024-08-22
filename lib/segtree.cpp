ed504eb582783aab8b28596929bcdc1f7c433a17
ht06359 <mightyworksmf@gmail.com>
2024-08-22 12:54:50 +0900

    commit

--- lib/segtree.cpp
+++ lib/segtree.cpp
@@ -4,18 +4,19 @@ using namespace std;
 
 // https://onlinejudge.u-aizu.ac.jp/status/users/ht06359/submissions/1/DSL_2_A/judge/9574552/C++17
 // https://onlinejudge.u-aizu.ac.jp/status/users/ht06359/submissions/1/DSL_2_B/judge/9574541/C++17
+// https://judge.yosupo.jp/submission/229994
+
 template<typename T, auto op, auto e>
 struct segment_tree {
-    int n, siz;
+    int n;
     vector<T> dat;
     segment_tree (int _n) {
-        n = _n;
-        siz = __lg(n) + 2;
-        dat.assign(1 << siz, e());
+        n = __lg(_n) + 1;
+        dat.assign(1 << (n+1), e());
     };
 
     void update(int i, T x) {
-        i += (1 << (siz-1));
+        i += (1 << n);
         dat[i] = x;
         while (i > 1) {
             i >>= 1;
@@ -24,11 +25,11 @@ struct segment_tree {
     }
 
     T get(int i) {
-        return dat[i + (1<<(siz-1))];
+        return dat[i + (1 << n)];
     }
 
     T prod(int l, int r) {
-        return _prod(l, r, 1, 0, 1<<(siz-1));
+        return _prod(l, r, 1, 0, 1 << n);
     }
 
     T prod_all() {
@@ -47,5 +48,5 @@ int64_t op(int64_t a, int64_t b) {
     return min(a, b);
 }
 int64_t e() {
-    return (1ll<<31)-1;
+    return (int64_t) 1e16;
 }
